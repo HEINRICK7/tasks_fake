e69ce27b4df17a63cb5dc1a5efee5bf41c92d019
HEINRICK7 <heinrickcostta@gmail.com>
2022-11-03 15:10:04 -0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,283 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import {
+  Button,
+  Form,
+  Input,
+  Popconfirm,
+  Table,
+  Divider,
+  Image,
+  Checkbox,
+  Row,
+  Col,
+} from "antd";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { SearchOutlined, UserAddOutlined } from "@ant-design/icons";
+const EditableContext = React.createContext(null);
+const EditableRow = ({ index, ...props }) => {
+  const [form] = Form.useForm();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
   );
-}
-
+};
+const EditableCell = ({
+  title,
+  editable,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef(null);
+  const form = useContext(EditableContext);
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+    }
+  }, [editing]);
+  const toggleEdit = () => {
+    setEditing(!editing);
+    form.setFieldsValue({
+      [dataIndex]: record[dataIndex],
+    });
+  };
+  const save = async () => {
+    try {
+      const values = await form.validateFields();
+      toggleEdit();
+      console.log("Save failed:");
+      handleSave({
+        ...record,
+        ...values,
+      });
+    } catch (errInfo) {
+      console.log("Save failed:", errInfo);
+    }
+  };
+  let childNode = children;
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{
+          margin: 0,
+        }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{
+          paddingRight: 24,
+        }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  }
+  return <td {...restProps}>{childNode}</td>;
+};
+const App = () => {
+  const [dataSource, setDataSource] = useState([]);
+  const [count, setCount] = useState(2);
+  const [results, setResults] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectionType, setSelectionType] = useState("checkbox");
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        {selectedRows}
+      );
+    },
+    getCheckboxProps: (record) => ({
+      disabled: record.name === "Disabled User",
+      // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+  useEffect(() => {
+    fetch(`https://my-json-server.typicode.com/HEINRICK7/fakeApi/db`)
+      .then((res) => res.json())
+      .then((data) => {
+        setResults(data.pacientes);
+      });
+  }, []);
+  const filteredServidor = results.filter((result) => {
+    return (
+      result.cpf.toLowerCase().includes(search.toLowerCase()) ||
+      result.nome.toLowerCase().includes(search.toLowerCase()) ||
+      result.numero_prontuario.includes(search)
+    );
+  });
+  console.log(filteredServidor);
+  const handleDelete = (key) => {
+    const newData = dataSource.filter((item) => item.key !== key);
+    setDataSource(newData);
+  };
+  const defaultColumns = [
+    {
+      title: "Nº prontuário",
+      dataIndex: "numero_prontuario",
+      width: "30%",
+      editable: true,
+    },
+    {
+      title: "Paciente",
+      dataIndex: "nome",
+      editable: true,
+    },
+    {
+      title: "Data de nascimento",
+      dataIndex: "data_nascimento",
+      editable: true,
+    },
+    {
+      title: "Procedimento",
+      dataIndex: "procedimento",
+      editable: true,
+    },
+    {
+      title: "operation",
+      dataIndex: "operation",
+      render: (_, record) =>
+        dataSource.length >= 1 ? (
+          <Popconfirm
+            title="Sure to delete?"
+            onConfirm={() => handleDelete(record.key)}
+          >
+            <a>Delete</a>
+          </Popconfirm>
+        ) : null,
+    },
+  ];
+  const handleAdd = () => {
+    const newData = {
+      key: count,
+      numero_prontuario: filteredServidor[0].numero_prontuario,
+      nome: filteredServidor[0].nome,
+      data_nascimento: filteredServidor[0].data_nascimento,
+      procedimento: filteredServidor[0].procedimento,
+      images: filteredServidor[0].images
+    };
+    setDataSource([...dataSource, newData]);
+    setCount(count + 1);
+  };
+  const handleSave = (row) => {
+    const newData = [...dataSource];
+    const index = newData.findIndex((item) => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    setDataSource(newData);
+  };
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+  const columns = defaultColumns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
+  return (
+    <>
+      <form>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            background: "grey",
+            justifyContent: "space-between",
+            padding: "20px",
+          }}
+        >
+          <h1 style={{ color: "snow" }}>Associar Paciente</h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Input
+              size="large"
+              placeholder="Digite"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              suffix={<SearchOutlined />}
+            />
+            <Button
+              onClick={handleAdd}
+              type="primary"
+              icon={<UserAddOutlined />}
+              size="large"
+            />
+          </div>
+        </div>
+        <div>
+          <Table
+            components={components}
+            rowSelection={{
+              type: selectionType,
+              ...rowSelection,
+            }}
+            rowClassName={() => "editable-row"}
+            bordered
+            dataSource={dataSource}
+            columns={columns}
+          />
+        </div>
+        <Divider />
+        {dataSource.length !== 0 ? (
+          <Image.PreviewGroup>
+            <Checkbox.Group
+              style={{
+                width: "100%",
+              }}
+              onChange={rowSelection}
+            >
+              <Row>
+                <Col span={8}>
+                  {dataSource?.map((res) =>
+                    res.images.map((res) => <Checkbox value={res} />)
+                  )}
+                </Col>
+              </Row>
+            </Checkbox.Group>
+            {dataSource?.map((res) =>
+              res.images.map((res) => <Image width={200} src={res.image} />)
+            )}
+          </Image.PreviewGroup>
+        ) : null}
+      </form>
+    </>
+  );
+};
 export default App;
